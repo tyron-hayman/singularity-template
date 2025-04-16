38b666283af8544ba7677ea52d6286e52dce5a05
Tyron H <tyronh@Tyrons-Mac-mini.local>
2025-04-16 00:01:41 -0700

    Hero component updates, sanity installation, animation addition, env variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,17 @@ import Image from "next/image";
 import HeroComponent from "@/components/HeroComponent";
 
 export default function Home() {
+  const heroNav: Array<{ id: number, text: string, anchor : string}> = [
+    { id: 1, text : 'About', anchor : '#about' },
+    { id: 2, text : 'Work', anchor : '#work' }
+  ]
+
   return (
-    <div className="w-full py-80">
-      <HeroComponent />
+    <div className="w-full py-40">
+      <HeroComponent
+        title="Hi! I am Tyron, a passionate & ambitious web developer."
+        nav={heroNav}
+      />
     </div>
   );
 }
--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,9 +1,54 @@
-const HeroComponent = ({ title = "This is a title" } : { title? : string }) => {
-    return(
-        <div className="container mx-auto">
-            <h1 className="mainHeadings text-8xl font-black text-[var(--color-foreground)]">{title}</h1>
-        </div>
-    )
-}
+import * as motion from "motion/react-client";
+
+const HeroComponent = ({
+  title = "This is a title",
+  nav,
+}: {
+  title?: string;
+  nav?: Array<{ id: number; text: string; anchor: string }>;
+}) => {
+  return (
+    <div className="container mx-auto">
+      <motion.h1
+        initial={{ y: 100, opacity: 0 }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          transition: { duration: 1, ease: "easeOut" },
+        }}
+        className="mainHeadings text-[7vw] leading-[7vw] font-black text-[var(--color-foreground)]"
+      >
+        {title}
+      </motion.h1>
+      <motion.ul
+        initial={{ y: 100, opacity: 0 }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          transition: { duration: 1, delay: 0.25, ease: "easeOut" },
+        }}
+        className="my-20 flex gap-5 items-center"
+      >
+        {nav?.map(
+          (
+            navItem: { id: number; text: string; anchor: string },
+            index: number
+          ) => {
+            return (
+              <li key={navItem.id}>
+                <a
+                  href={navItem.anchor}
+                  className="border-[var(--color-foreground)] border-solid border-2 rounded-full text-[var(--color-foreground)] text-2xl block px-10 py-2"
+                >
+                  {navItem.text}
+                </a>
+              </li>
+            );
+          }
+        )}
+      </motion.ul>
+    </div>
+  );
+};
 
 export default HeroComponent;
