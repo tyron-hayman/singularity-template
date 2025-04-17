4b026149f12d6528d4439aa2c2e183d50de536bf
Tyron H <tyronh@Tyrons-Mac-mini.local>
2025-04-16 22:10:27 -0700

    HeroComponent animatin, prop and correct type additions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
+"use client";
 import Image from "next/image";
 import HeroComponent from "@/components/HeroComponent";
+import Lenis from "lenis";
+import { useEffect } from "react";
 
 export default function Home() {
-  const heroNav: Array<{ id: number, text: string, anchor : string}> = [
-    { id: 1, text : 'About', anchor : '#about' },
-    { id: 2, text : 'Work', anchor : '#work' }
-  ]
+  const heroNav: Array<{ id: number; text: string; anchor: string }> = [
+    { id: 1, text: "About", anchor: "#about" },
+    { id: 2, text: "Work", anchor: "#work" },
+  ];
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+  },[]);
 
   return (
-    <div className="w-full py-40">
+    <div className="w-full py-40 mb-[100vh] relative">
       <HeroComponent
         title="Hi! I am Tyron, a passionate & ambitious web developer."
+        subTitle="From the beautiful city of Vancouver, BC."
         nav={heroNav}
       />
     </div>
--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -2,11 +2,15 @@ import * as motion from "motion/react-client";
 
 const HeroComponent = ({
   title = "This is a title",
+  subTitle = "This is a subtitle",
   nav,
 }: {
   title?: string;
+  subTitle?: string;
   nav?: Array<{ id: number; text: string; anchor: string }>;
 }) => {
+  const animationTiming: number = 0.75;
+
   return (
     <div className="container mx-auto">
       <motion.h1
@@ -14,39 +18,54 @@ const HeroComponent = ({
         animate={{
           y: 0,
           opacity: 1,
-          transition: { duration: 1, ease: "easeOut" },
+          transition: { duration: animationTiming, ease: "easeOut" },
         }}
         className="mainHeadings text-[7vw] leading-[7vw] font-black text-[var(--color-foreground)]"
       >
         {title}
       </motion.h1>
-      <motion.ul
+      <motion.h2
         initial={{ y: 100, opacity: 0 }}
         animate={{
           y: 0,
           opacity: 1,
-          transition: { duration: 1, delay: 0.25, ease: "easeOut" },
+          transition: { duration: animationTiming, delay: 0.25, ease: "easeOut" },
         }}
-        className="my-20 flex gap-5 items-center"
+        className="text-3xl leading-relaxed font-normal text-[var(--accent-color)] mt-5"
       >
+        {subTitle}
+      </motion.h2>
+      <ul className="block mt-20 flex gap-5 items-center sticky top-[20px]">
         {nav?.map(
           (
             navItem: { id: number; text: string; anchor: string },
             index: number
           ) => {
             return (
-              <li key={navItem.id}>
+              <motion.li
+                key={navItem.id}
+                initial={{ y: 100, opacity: 0 }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                  transition: {
+                    duration: animationTiming,
+                    delay: 0.275 * (index + 1),
+                    ease: "easeOut",
+                  },
+                }}
+              >
                 <a
                   href={navItem.anchor}
                   className="border-[var(--color-foreground)] border-solid border-2 rounded-full text-[var(--color-foreground)] text-2xl block px-10 py-2"
                 >
                   {navItem.text}
                 </a>
-              </li>
+              </motion.li>
             );
           }
         )}
-      </motion.ul>
+      </ul>
     </div>
   );
 };
