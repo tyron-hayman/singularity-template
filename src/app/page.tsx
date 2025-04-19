eda07078fa3507f515210b4743a610c41a8c6331
Tyron H <tyronh@Tyrons-Mac-mini.local>
2025-04-19 00:16:05 -0700

    Adding motion when user scrolls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import HeroComponent from "@/components/HeroComponent";
 import GlobalNav from "@/components/GlobalNav";
 import Lenis from "lenis";
 import { useEffect } from "react";
+import { useScroll } from "motion/react"
 
 export default function Home() {
   const heroNav: Array<{ id: number; text: string; anchor: string }> = [
     { id: 1, text: "About", anchor: "#about" },
     { id: 2, text: "Work", anchor: "#work" },
   ];
+  const { scrollYProgress } = useScroll()
 
   useEffect(() => {
     const lenis = new Lenis({
@@ -23,7 +25,7 @@ export default function Home() {
       <HeroComponent
         title="Hi! I am Tyron, a passionate & ambitious web developer."
         subTitle="From the beautiful city of Vancouver, BC."
-        nav={heroNav}
+        scrollProgress={scrollYProgress}
       />
     </div>
   );
--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,21 +1,24 @@
 import * as motion from "motion/react-client";
+import type { MotionValue } from "motion/react";
 import TextAnimation from "./TextAnimationComponent";
 import { MapPin } from "lucide-react";
+import { useTransform } from "motion/react";
 
 const HeroComponent = ({
   title = "This is a title",
   subTitle = "This is a subtitle",
-  nav,
+  scrollProgress,
 }: {
   title?: string;
   subTitle?: string;
-  nav?: Array<{ id: number; text: string; anchor: string }>;
+  scrollProgress?: any
 }) => {
   const animationTiming: number = 0.75;
+  const y = useTransform(scrollProgress, [0,1], [0,500]);
 
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="container">
+      <motion.div className="container" style={{ y }}>
         <h1 className="mainHeadings text-[7vw] leading-[7vw] font-black text-[var(--color-foreground)]">
           <TextAnimation phrase={title} />
         </h1>
@@ -34,7 +37,7 @@ const HeroComponent = ({
         >
           <MapPin className="inline-block" color="#F05454" strokeWidth={2} /> {subTitle}
         </motion.h2>
-      </div>
+      </motion.div>
     </div>
   );
 };
